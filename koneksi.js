b013b0c9b606fdc0c2c4f61900bba6ae0b2fa8aa
dgramadhan <62194600+dgramadhan@users.noreply.github.com>
2020-06-02 23:27:54 +0700

    Update koneksi.js

--- koneksi.js
+++ koneksi.js
@@ -1,10 +1,10 @@
 var mysql = require ('mysql');
 
 var db = mysql.createConnection({
-    host: "databases.000webhost.com",
-    user: "id13933173_database_pa",
-    password: "5ilpZq!<@$H>v!GM",
-    database: "id13933173_db_pa",
+    host: "remotemysql.com",
+    user: "wAJsibpSOv",
+    password: "lmQiJMOhzk",
+    database: "wAJsibpSOv"
     timezone : 'utc'
 
 });
