4e408baa839cd57bc1267b50f68c6c1afd7ee46e
dgramadhan <62194600+dgramadhan@users.noreply.github.com>
2020-06-03 19:54:49 +0700

    Update controller.js

--- controller.js
+++ controller.js
@@ -32,8 +32,11 @@ exports.sensor = function(req, res) {
     });
 };
 
+const moment = require('moment');
+
 exports.sensor_post = function(req, res) {
-    connection.query('INSERT INTO sensor (id_sensor, value_sensor, id_dev, waktu) VALUES (?,?,?,CURTIME())', [req.body.id_sensor, req.body.value_sensor, req.body.id_dev], function (error, rows, fields){
+	const today = moment().format('HH:mm:ss');
+    connection.query('INSERT INTO sensor (id_sensor, value_sensor, id_dev, waktu) VALUES (?,?,?,?)', [req.body.id_sensor, req.body.value_sensor, req.body.id_dev, today], function (error, rows, fields){
         if(error){
             console.log(error)
         } 
